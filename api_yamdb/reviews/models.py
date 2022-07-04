02fee9487d1faa9a8f6363181d6ee071d2e36903
yorriyurri <skierorama@gmail.com>
2022-07-04 17:24:12 +0300

    check project with flake8, version 7

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,8 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from users.models import User
 
 from .validators import validate_year
-from users.models import User
 
 
 class Category(models.Model):
