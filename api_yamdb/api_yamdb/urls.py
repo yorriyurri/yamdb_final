31f1d49d92f7d42d4aeb37c0a256f37bce47ca70
yorriyurri <skierorama@gmail.com>
2022-07-04 17:11:01 +0300

    check project with flake8, version 2

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from django.views.generic import TemplateView
-from django.urls import include, path
-
 from api.urls import urlpatterns
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
